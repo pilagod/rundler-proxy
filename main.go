2805ac640980205228bc1726fa8043590daaeefa
pilagod <pilagooood@gmail.com>
2024-05-29 15:47:32 +0800

    fetch EntryPoints at startup

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"reflect"
+)
+
+var (
+	rundlerV06Proxy *httputil.ReverseProxy
+	entryPointsV06  []string
 )
 
 func main() {
@@ -16,10 +22,17 @@ func main() {
 	if err != nil {
 		panic("Invalid url: RUNDLER_V0_6")
 	}
-	rundlerV06Proxy := httputil.NewSingleHostReverseProxy(rundlerV06Url)
+	log.Println("RunderV06:", rundlerV06Url.String())
+	rundlerV06Proxy = httputil.NewSingleHostReverseProxy(rundlerV06Url)
+
+	entryPointsV06, err := getSupportedEntryPoints(rundlerV06Url)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("EntryPointsV06:", entryPointsV06)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", createProxyHandler(rundlerV06Proxy))
+	mux.HandleFunc("/", createProxyHandler())
 
 	log.Println("Listening on :3000")
 	if err = http.ListenAndServe(":3000", mux); err != nil {
@@ -34,9 +47,43 @@ type JSONRPCRequest struct {
 	Params  []interface{} `json:"params"`
 }
 
-func createProxyHandler(
-	rundlerV06Proxy *httputil.ReverseProxy,
-) func(http.ResponseWriter, *http.Request) {
+type JSONRPCResponse struct {
+	ID      int         `json:"id"`
+	JSONRPC string      `json:"jsonrpc"`
+	Result  interface{} `json:"result"`
+}
+
+func getSupportedEntryPoints(url *url.URL) (entryPoints []string, err error) {
+	r := JSONRPCRequest{
+		ID:      1,
+		JSONRPC: "2.0",
+		Method:  "eth_supportedEntryPoints",
+		Params:  make([]interface{}, 0),
+	}
+	var data bytes.Buffer
+	if err = json.NewEncoder(&data).Encode(r); err != nil {
+		return
+	}
+	res, err := http.Post(url.String(), "application/json", &data)
+	if err != nil {
+		return
+	}
+	var result JSONRPCResponse
+	if err = json.NewDecoder(res.Body).Decode(&result); err != nil {
+		return
+	}
+	v := reflect.ValueOf(result.Result)
+	if v.IsZero() {
+		return
+	}
+	entryPoints = make([]string, v.Len())
+	for i := 0; i < v.Len(); i++ {
+		entryPoints[i] = v.Index(i).Interface().(string)
+	}
+	return
+}
+
+func createProxyHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		body := new(bytes.Buffer)
 		bodyReader := io.TeeReader(r.Body, body)
